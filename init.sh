def6d7c545641168cb17faa76ab8f30ba2b99a13
Paul Everitt <paul@agendaless.com>
2015-07-04 09:12:28 -0400

    Put the conf as an option.

--- init.sh
+++ init.sh
@@ -5,12 +5,13 @@
 set -e
 
 PATH="${PATH}:/usr/local/sbin:/usr/sbin:/sbin"
-DAEMON="${HOME}/memcachedconfs/bin/supervisord -c ${HOME}/memcachedconfs/production.conf"
-CTL="${HOME}/memcachedconfs/bin/supervisorctl -c ${HOME}/memcachedconfs/production.conf"
+DAEMON="${HOME}/memcachedconfs/bin/supervisord"
+CTL="${HOME}/memcachedconfs/bin/supervisorctl"
+CONF="${HOME}/memcachedconfs/production.conf"
 PIDFILE="/run/local/supervisord-memcachedconfs.pid"
 
 start() {
-    start-stop-daemon --start -p "$PIDFILE" -i -- "$DAEMON"
+    start-stop-daemon --start -p "$PIDFILE" -i -- "$DAEMON" -c "$CONF"
 }
 
 stop() {
